9cbac5b01de6f5a9a4203d075df78e40e86853bb
Jan Parthemore <janparthemore@gmail.com>
2017-07-12 14:49:35 -0400

    added check for file not found

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 8080;
 
 //middleware
 const logger = require('./middleware/logger');
+const notFound = require('./middleware/404.js');
 //router
 const weatherRouter = require('./routers/weather.router');
 
@@ -15,7 +16,7 @@ server.get('/',(req,resp)=>{
   resp.send('It Works!');
 });
 
-
+server.use(notFound);
 server.listen(port,()=>{
   console.log('Listening on port: ', port);
 });
--- middleware/404.js
+++ middleware/404.js
@@ -1,4 +1,7 @@
 const notFound = (req,resp,next)=>{
-  resp.status(404).json();
-
+  resp.status(404).json({
+    message: "Cannot find what you are looking for"
+  });
 };
+
+module.exports = notFound;
